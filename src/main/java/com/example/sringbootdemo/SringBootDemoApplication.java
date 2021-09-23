7c5a43445cd98ce45569a6f4768437f9a017e896
wuhaohao <15037966819>
2021-09-23 15:45:46 +0800

    测试

--- src/main/java/com/example/sringbootdemo/SringBootDemoApplication.java
+++ src/main/java/com/example/sringbootdemo/SringBootDemoApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SringBootDemoApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(SringBootDemoApplication.class, args);
     }
